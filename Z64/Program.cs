910773f4f52c9ae7e557f855122b42c1fd1deaad
Ekaterina <kmatway@yandex.ru>
2023-01-09 14:42:45 +0500

    решение задачи 54

---
+++ Z64/Program.cs
@@ -0,0 +1,10 @@
+﻿// ача 64: Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+
+void Numbers(int m, int n)
+{
+    if (n > m) Numbers(m,n-1);
+    Console.WriteLine($"{n}");
+    if(m > n) Numbers(m,n + 1);
+}
+Numbers(10,1);
